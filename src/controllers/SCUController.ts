668d492172cb328a418e756441633cfc6e391990
Antonio Gamiz <antoniogamiz10@gmail.com>
2020-03-05 14:28:57 +0100

    Añade SCUController, #6

---
+++ src/controllers/SCUController.ts
@@ -0,0 +1,9 @@
+import Menu from "../models/Menu";
+
+class SCUController {
+  consultMenu(day: Date, place): Menu {
+    return new Menu();
+  }
+}
+
+export default SCUController;
--- src/models/DayMenu.ts
+++ src/models/DayMenu.ts
@@ -1,7 +1,33 @@
 import Menu from "./Menu";
 
-interface DayMenu {
-  ptsMenu: Menu[];
-  fuenteNuevaMenu: Menu[];
-  date: Date;
+class DayMenu {
+  private _ptsMenu: Menu[];
+  private _fuenteNuevaMenu: Menu[];
+  private _date: Date;
+
+  // getters and setters
+
+  get date(): Date {
+    return new Date();
+  }
+
+  set date(date: Date) {
+    this._date = date;
+  }
+
+  get ptsMenu(): Menu[] {
+    return [];
+  }
+
+  set ptsMenu(menus: Menu[]) {
+    this._ptsMenu = menus;
+  }
+
+  get fuenteNueva(): Menu[] {
+    return [];
+  }
+
+  set fuenteNueva(menus: Menu[]) {
+    this._fuenteNuevaMenu = menus;
+  }
 }
