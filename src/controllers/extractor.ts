aec0ce0652a9ffe3d0b3cc2f9cb6b3e735cb276e
Antonio Gamiz <antoniogamiz10@gmail.com>
2020-03-06 14:46:15 +0100

    Añade implementacion de 'extractor', #9

--- src/controllers/extractor.ts
+++ src/controllers/extractor.ts
@@ -1,5 +1,24 @@
-function httpRequest(url: string): string {
-  return "";
+import axios from "axios";
+
+export async function httpRequest(
+  url: string,
+  n: number = 3,
+  client: Function = axios,
+  options = {
+    method: "get",
+    timeout: 500,
+    responseType: "document"
+  }
+) {
+  let error;
+  for (let i = 0; i < n; i++) {
+    try {
+      return await client({ ...options, url: url });
+    } catch (err) {
+      error = err;
+    }
+  }
+  throw new SCUPageNotAvailable(error.message);
 }
 
 class SCUPageNotAvailable extends Error {}
