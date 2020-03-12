cc22cfe486a1c4e28eaafcb4e1547153bf992f4e
Antonio Gamiz <antoniogamiz10@gmail.com>
2020-03-12 13:41:07 +0100

    Añade test para extractor #9

--- src/__tests__/httpRequest.test.ts
+++ src/__tests__/httpRequest.test.ts
@@ -1,7 +1,7 @@
-let suma = (a, b) => {
-  return 3;
-};
+const extractor = require("../controllers/extractor");
 
 test("sumar 1 + 2 es igual a 3", () => {
-  expect(suma(1, 2)).toBe(3);
+  //   expect(extractor("https://google.es")).toBe("Dddd");
+
+  console.log(extractor("https://google.es"));
 });
