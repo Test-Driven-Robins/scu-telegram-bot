9ed6eed143c3c9f54857ce1a704dae43397703f0
AlexMenor <alex4menor@gmail.com>
2020-03-20 10:30:24 +0100

    modifica configuración de jest para que testee la cobertura de services

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/__tests__/**/*.test.ts"]
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  collectCoverageFrom: ['**/*.ts', '!**/index.ts'],
 };
