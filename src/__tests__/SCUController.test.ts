11b11e89477aa7722e37399245287280e35c583e
AlexMenor <alex4menor@gmail.com>
2020-03-20 09:59:27 +0100

    añade menus.json y test de menu de mañana para el controlador

--- src/__tests__/SCUController.test.ts
+++ src/__tests__/SCUController.test.ts
@@ -1,30 +1,38 @@
-import DayMenu from "../models/DayMenu";
-import Dish from "../models/Dish";
-import Menu from "../models/Menu";
-import { SCUController, Weekdays } from "../controllers/SCUController";
+import DayMenu from '../models/DayMenu';
+import Dish from '../models/Dish';
+import Menu from '../models/Menu';
+import { SCUController, Weekdays } from '../controllers/SCUController';
 
 const mockSCUService = {
   async getDayMenu(day: number, month: number, year: number): Promise<DayMenu> {
-    const platoAlpujarreño = new Dish("Plato alpujarreño");
-    const macarrones = new Dish("Plato alpujarreño");
-    const cuajadaDeCarnaval = new Dish("Cuajada de carnaval", ["Gluten"]);
+    const platoAlpujarrenio = new Dish('Plato alpujarreño');
+    const macarrones = new Dish('Plato alpujarreño');
+    const cuajadaDeCarnaval = new Dish('Cuajada de carnaval', ['Gluten']);
 
     const menu = new Menu(
       macarrones,
-      platoAlpujarreño,
+      platoAlpujarrenio,
       cuajadaDeCarnaval,
-      "Vino tinto",
-      false
+      'Vino tinto',
+      false,
     );
 
     return new DayMenu([menu], [menu], 10, 2, 2020);
-  }
+  },
 };
 
-it("should return a day menu", async () => {
+it('should return todays menu', async () => {
   const controller = new SCUController(mockSCUService);
 
-  const dayMenu: DayMenu = await controller.getDayMenu(Weekdays.Monday);
+  const dayMenu: DayMenu = await controller.getToday();
+
+  expect(dayMenu).toEqual(await mockSCUService.getDayMenu(10, 2, 2020));
+});
+
+it('should return tomorrows menu', async () => {
+  const controller = new SCUController(mockSCUService);
+
+  const dayMenu: DayMenu = await controller.getTomorrow();
 
   expect(dayMenu).toEqual(await mockSCUService.getDayMenu(10, 2, 2020));
 });
